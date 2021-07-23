f37c4289031e6ecabfe7ac988b4e7c90ccd373ed
Jesus Larrubia <jesus.larrubia@clevertech.biz>
2021-07-23 20:38:27 +0200

    statefulset-testrunner-support: find namespaces defined by statefulSets and fix duplicated namespaces

--- pkg/k8sClient/namespaces.go
+++ pkg/k8sClient/namespaces.go
@@ -10,21 +10,43 @@ import (
 //ListNamespaces lists the currently active namespaces.
 func (k KubeClient) ListNamespaces(labelSelector string) ([]string, error) {
 
+	list := []string{}
+	// Used to avoid duplicates.
+	listMap := make(map[string]bool)
 	options := metav1.ListOptions{
 		LabelSelector: labelSelector,
 	}
 
+	// Try to find the app among Deployments.
 	deployments, err := k.clientSet.AppsV1().Deployments("").List(options)
 
 	if err != nil {
 		return []string{}, err
 	}
 
-	list := []string{}
-	for _, d := range deployments.Items {
-		list = append(list, d.Namespace)
+	// If there is no luck, try StatefulSets.
+	if len(deployments.Items) > 0 {
+		for _, d := range deployments.Items {
+			if _, ok := listMap[d.Namespace]; !ok {
+				listMap[d.Namespace] = true
+				list = append(list, d.Namespace)
+			}
+
+		}
+	} else {
+		statefulSets, err := k.clientSet.AppsV1().StatefulSets("").List(options)
+
+		if err != nil {
+			return []string{}, err
+		}
+
+		for _, s := range statefulSets.Items {
+			if _, ok := listMap[s.Namespace]; !ok {
+				listMap[s.Namespace] = true
+				list = append(list, s.Namespace)
+			}
+		}
 	}
 
 	return list, nil
-
 }
